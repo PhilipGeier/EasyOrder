f3f7d5e2e33561cab7b9391606ea358a734ce504
PhilipGeier <philipgeieru@gmail.com>
2022-03-31 00:11:09 +0200

    Console

---
+++ OrderingSystem.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using OrderingSystem.Domain;
+using OrderingSystem.Logic;
+using OrderingSystem.Logic.Json;
+
+namespace OrderingSystem.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tableLogic = new TableLogic();
+            
+            tableLogic.CreateTable("1", WpfInfo.Create(150, 150, 50,50,"Ellipse"));
+            tableLogic.CreateTable("2", WpfInfo.Create(250, 400, 50,50,"Ellipse"));
+            tableLogic.CreateTable("3", WpfInfo.Create(400, 200, 50,50,"Ellipse"));
+            tableLogic.CreateTable("5", WpfInfo.Create(400, 400, 50,50,"Ellipse"));
+            
+        }
+    }
+}
